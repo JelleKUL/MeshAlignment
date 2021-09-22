e65cf95d6ce50ceb5571a450032b2231b7638681
Jelle Vermandere <jelle.vermandere@kuleuven.be>
2021-09-22 14:23:42 +0200

    update

---
+++ Runtime/CoordinateConverter.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JelleKUL.MeshAlignment
+{
+    // the different coordinates to have your measurements in
+    public enum CoordinateSystem { Lambert72, Lambert2008, Spherical }
+
+    public class CoordinateConverter : MonoBehaviour
+    {
+
+        //todo add more coordinates
+        /// <summary>
+        /// Convert a position to another coordinate system
+        /// </summary>
+        /// <param name="input">the position in (x,y,z) or (long, lat, height)</param>
+        /// <param name="fromCoordinate">the coordinatesystem the input is in</param>
+        /// <param name="toCoordinate">the coordinate system the output has to be</param>
+        /// <returns>converted coordinates in the toCoordinate system</returns>
+        public static Vector3 ConvertCoordinates(Vector3 input, CoordinateSystem fromCoordinate, CoordinateSystem toCoordinate)
+        {
+            switch (fromCoordinate)
+            {
+                case CoordinateSystem.Spherical:
+                    switch (toCoordinate)
+                    {
+                        case CoordinateSystem.Lambert72:
+                            Vector2 coordinates = SphereToLambert72(input.x, input.y);
+                            return new Vector3(coordinates.x, coordinates.y, input.z);
+
+                        //todo add more cases
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                //todo add more cases
+
+                default:
+                    break;
+            }
+
+            return input;
+        }
+
+
+        /// <summary>
+        /// Concert spherical coortinates to lambert72 coordinates
+        /// </summary>
+        /// <param name="lng">the longitude in decimal belgium Datum</param>
+        /// <param name="lat">the latitude in decimal belgium Datum</param>
+        /// <returns>the lambert72 x & y coordinates</returns>
+        public static Vector2 SphereToLambert72(float lng, float lat)
+        {
+
+            float LongRef = 0.076042943f;      //=4°21'24"983
+            float bLamb = 6378388 * (1 - (1 / 297.0f));
+            float aCarre = Mathf.Pow(6378388,2);
+            float eCarre = (aCarre - bLamb * bLamb) / aCarre;
+            float KLamb = 11565915.812935f;
+            float nLamb = 0.7716421928f;
+
+            float eLamb = Mathf.Sqrt(eCarre);
+            float eSur2 = eLamb / 2f;
+
+            //conversion to radians
+            lat *= (Mathf.PI / 180f);
+            lng *= (Mathf.PI / 180f);
+
+
+            float eSinLatitude = eLamb * Mathf.Sin(lat);
+            float TanZDemi = (Mathf.Tan((Mathf.PI / 4f) - (lat / 2f))) * Mathf.Pow((1 + eSinLatitude) / (1 - eSinLatitude), eSur2);
+
+            float RLamb = KLamb * Mathf.Pow(TanZDemi, nLamb);
+
+            float Teta = nLamb * (lng - LongRef);
+
+            float x = 150000  + 0.01256f + RLamb* Mathf.Sin(Teta - 0.000142043f);
+            float y = 5400000 + 88.4378f - RLamb* Mathf.Cos(Teta - 0.000142043f);
+
+            return new Vector2(x, y);
+        }
+    }
+}
+
---
+++ Runtime/GlobalPosition.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JelleKUL.MeshAlignment
+{
+    /// <summary>
+    /// functions to get the global position of the device
+    /// </summary>
+    public class GlobalPosition : MonoBehaviour
+    {
+        [Tooltip("The current coordinate system to store the position in")]
+        public CoordinateSystem coordinateSystem = CoordinateSystem.Lambert72;
+        public Vector3 position { get; private set; } = Vector3.zero; //the position of the device
+        public float errorRadius { get; private set; }  = 0f; //the error radius of the device
+        public LocationInfo lastLocation { get; private set; } //the last locationinfo, containing all kinds of info
+        public bool hasData { get; private set; } = false; //has there been a succesfull connect
+
+        [SerializeField]
+        [Tooltip("Log all the Data and processes")]
+        private bool logData = false;
+        [SerializeField]
+        [Tooltip("Start the location finding at start")]
+        private bool findLocationAtStart = false;
+        [SerializeField]
+        [Tooltip("The maximum amount of time the device should check for a location")]
+        [Range(0,60)]
+        private int maxWaitTime = 10;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (findLocationAtStart) StartFindGlobalPosition();
+        }
+
+        public void StartFindGlobalPosition()
+        {
+            StartCoroutine(FindGlobalPosition(OnPositionFound));
+        }
+
+        //todo check if device supports location getting
+        public IEnumerator FindGlobalPosition(System.Action<bool> callbackOnFinish)
+        {
+            Log("Starting Location search");
+            bool gotLocation = false;
+
+
+            // First, check if user has location service enabled
+            if (!Input.location.isEnabledByUser)
+                callbackOnFinish(gotLocation);
+
+            // Start service before querying location
+            Input.location.Start();
+
+            Log("LocationSatus: " + Input.location.status);
+
+            // Wait until service initializes
+            int maxWait = maxWaitTime;
+            while ((Input.location.status == LocationServiceStatus.Initializing || Input.location.status == LocationServiceStatus.Stopped) && maxWait > 0)
+            {
+                yield return new WaitForSeconds(1);
+                Log("Connecting...");
+                maxWait--;
+            }
+
+            // Service didn't initialize in 20 seconds
+            if (maxWait < 1)
+            {
+                Log("Timed out");
+                callbackOnFinish(gotLocation);
+            }
+
+            Log("LocationSatus: " + Input.location.status);
+
+            // Connection has failed
+            if (Input.location.status == LocationServiceStatus.Failed || Input.location.status == LocationServiceStatus.Stopped)
+            {
+                Log("Unable to determine device location");
+                callbackOnFinish(gotLocation);
+            }
+            else
+            {
+                // Access granted and location value could be retrieved
+                hasData = true;
+                gotLocation = true;
+
+                lastLocation = Input.location.lastData;
+                errorRadius = Mathf.Max(Input.location.lastData.horizontalAccuracy, Input.location.lastData.verticalAccuracy);
+
+                //convert the longitude and latitude to lambert
+                Vector3 gpsPos = new Vector3(Input.location.lastData.longitude, Input.location.lastData.latitude, Input.location.lastData.altitude);
+                position = CoordinateConverter.ConvertCoordinates(gpsPos, CoordinateSystem.Spherical, coordinateSystem);
+                
+            }
+
+            // Stop service if there is no need to query location updates continuously
+            Input.location.Stop();
+
+            //send the succes back to the callback
+            callbackOnFinish(gotLocation);
+        }
+
+        void OnPositionFound(bool succes)
+        {
+            if (succes)
+            {
+                Log("The device's current location = " + position + "\t With an error radius of: " + errorRadius + "m");
+            }
+            else
+            {
+                Log("Unable to determine device location");
+            }
+        }
+
+        void Log(string log)
+        {
+            if(logData) Debug.Log("<color=orange>" + name + ": </color>" + log);
+        }
+    }
+}
