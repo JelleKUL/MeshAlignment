b47305dc609e680b2f58ee0b2888e0351c45b90a
Jelle Vermandere <jelle.vermandere@kuleuven.be>
2021-11-10 12:32:22 +0100

    http client update

--- Runtime/Communication/HttpClient.cs
+++ Runtime/Communication/HttpClient.cs
@@ -71,5 +71,46 @@ namespace JelleKUL.MeshAlignment
                 return default;
             }
         }
+
+        public async Task<string> PostFiles(string url, object data)
+        {
+            try
+            {
+                string serializedData = (data.GetType() == typeof(string)) ? (string)data : JsonUtility.ToJson(data);
+
+                Debug.Log(serializedData);
+
+                /*
+                using var www = UnityWebRequest.Post(url, serializedData);
+
+                www.SetRequestHeader("Content-Type", "application/json");
+                */
+
+                var www = new UnityWebRequest(url, "POST");
+                byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(serializedData);
+                www.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+                www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+                www.SetRequestHeader("Content-Type", "application/json");
+
+                Debug.Log(www.url);
+
+                var operation = www.SendWebRequest();
+
+                while (!operation.isDone)
+                    await Task.Yield();
+
+                if (www.result != UnityWebRequest.Result.Success)
+                    Debug.LogError($"Failed: {www.error}");
+
+                var result = www.downloadHandler.text;
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"{nameof(Get)} failed: {ex.Message}");
+                return default;
+            }
+        }
     }
 }
